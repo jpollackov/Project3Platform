d8be51e7b46fd7db00044d87e2de7a7cce9678c9
jpollackov <jpollac1@villanova.edu>
2023-12-06 14:35:19 -0500

    added alerts for wrong values when adding an item

--- dataEntryView.swift
+++ dataEntryView.swift
@@ -18,42 +18,61 @@ struct DataEntryView: View {
     @State private var dorm = ""
     @State private var image = ""
     @State private var isAlertPresented = false
-    
+    @State private var isPriceErrorPresented = false
+    @State private var isPhoneNumberErrorPresented = false
+
+    var isInputValid: Bool {
+        return !item.isEmpty && !itemPrice.isEmpty && !renterName.isEmpty && !phoneNumber.isEmpty && !dorm.isEmpty && !image.isEmpty
+    }
+
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             Text("Enter Information:")
                 .font(.title)
                 .foregroundColor(.blue)
-            
+
             TextField("Item", text: $item)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
-            
+
             TextField("Price of Item", text: $itemPrice)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
-            
+                .keyboardType(.decimalPad)
+
             TextField("Full Name", text: $renterName)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .keyboardType(.default)
-            
+
             TextField("Phone Number", text: $phoneNumber)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .keyboardType(.phonePad)
-            
+
             TextField("Dorm", text: $dorm)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
-            
+
             TextField("Image", text: $image)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
-            
+
             Spacer()
+
             Button(action: {
-                if !item.isEmpty && !itemPrice.isEmpty && !renterName.isEmpty && !phoneNumber.isEmpty && !dorm.isEmpty && !image.isEmpty {
-                    let newItem = Item(name: item, itemPrice: itemPrice, renterName: renterName, renterPhoneNum: phoneNumber, dormBuilding: dorm, image: image)
-                    addItem(newItem)
-                    presentationMode.wrappedValue.dismiss()
-                } else {
+                guard isInputValid else {
                     isAlertPresented = true
+                    return
                 }
+
+                guard let price = Double(itemPrice) else {
+                    isPriceErrorPresented = true
+                    return
+                }
+
+                guard let _ = Int(phoneNumber) else {
+                    isPhoneNumberErrorPresented = true
+                    return
+                }
+
+                let newItem = Item(name: item, itemPrice: "\(price)", renterName: renterName, renterPhoneNum: phoneNumber, dormBuilding: dorm, image: image)
+                addItem(newItem)
+                presentationMode.wrappedValue.dismiss()
             }) {
                 RectangleButton(color: Color.blue, title: "Confirm")
             }
@@ -67,8 +86,22 @@ struct DataEntryView: View {
                 dismissButton: .default(Text("OK"))
             )
         }
+        .alert(isPresented: $isPriceErrorPresented) {
+            Alert(
+                title: Text("Invalid Price"),
+                message: Text("Please enter a valid number for the price."),
+                dismissButton: .default(Text("OK"))
+            )
+        }
+        .alert(isPresented: $isPhoneNumberErrorPresented) {
+            Alert(
+                title: Text("Invalid Phone Number"),
+                message: Text("Please enter a valid phone number."),
+                dismissButton: .default(Text("OK"))
+            )
+        }
     }
-    
+
     struct DataEntryView_Previews: PreviewProvider {
         static var previews: some View {
             DataEntryView(addItem: { _ in }, rentalsManager: RentalsManager())
