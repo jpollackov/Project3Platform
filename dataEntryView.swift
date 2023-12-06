54b6ffff598242569eadb004a5b5768f93f6c9a6
rtloftus <rtloftu01@villanova.edu>
2023-12-06 14:29:08 -0500

    made all fields required

--- dataEntryView.swift
+++ dataEntryView.swift
@@ -17,54 +17,61 @@ struct DataEntryView: View {
     @State private var phoneNumber = ""
     @State private var dorm = ""
     @State private var image = ""
-
-    var isInputValid: Bool {
-        return !item.isEmpty && !itemPrice.isEmpty && !renterName.isEmpty && !phoneNumber.isEmpty && !dorm.isEmpty
-    }
-
+    @State private var isAlertPresented = false
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             Text("Enter Information:")
                 .font(.title)
                 .foregroundColor(.blue)
-
+            
             TextField("Item", text: $item)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
-
+            
             TextField("Price of Item", text: $itemPrice)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
-
+            
             TextField("Full Name", text: $renterName)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .keyboardType(.default)
-
+            
             TextField("Phone Number", text: $phoneNumber)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .keyboardType(.phonePad)
-
+            
             TextField("Dorm", text: $dorm)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
-
+            
             TextField("Image", text: $image)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
-
+            
             Spacer()
-
             Button(action: {
-                guard isInputValid else {
-                    // Show an alert or some indication that all fields must be filled
-                    return
+                if !item.isEmpty && !itemPrice.isEmpty && !renterName.isEmpty && !phoneNumber.isEmpty && !dorm.isEmpty && !image.isEmpty {
+                    let newItem = Item(name: item, itemPrice: itemPrice, renterName: renterName, renterPhoneNum: phoneNumber, dormBuilding: dorm, image: image)
+                    addItem(newItem)
+                    presentationMode.wrappedValue.dismiss()
+                } else {
+                    isAlertPresented = true
                 }
-
-                let newItem = Item(name: item, itemPrice: itemPrice, renterName: renterName, renterPhoneNum: phoneNumber, dormBuilding: dorm, image: image)
-                addItem(newItem)
-                presentationMode.wrappedValue.dismiss()
             }) {
                 RectangleButton(color: Color.blue, title: "Confirm")
             }
             .padding()
-            .disabled(!isInputValid) // Disable button if input is not valid
         }
         .padding()
+        .alert(isPresented: $isAlertPresented) {
+            Alert(
+                title: Text("Incomplete Fields"),
+                message: Text("Please fill in all fields."),
+                dismissButton: .default(Text("OK"))
+            )
+        }
+    }
+    
+    struct DataEntryView_Previews: PreviewProvider {
+        static var previews: some View {
+            DataEntryView(addItem: { _ in }, rentalsManager: RentalsManager())
+        }
     }
 }
